15eea5754355da92ef98a41e33e4bbafdf7c6bc5
Mayank <mayank.srivastava@qualtechedge.com>
2023-01-03 16:33:09 +0530

    changes

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/AlienDBex'
+
+const app = express()
+
+//  {useNewUrlParser:true} - used in case the url is 
+// not deprecated
+mongoose.connect(url, {useNewUrlParser:true,
+    useUnifiedTopology: true,
+});
+// below used to get hold of the db connection
+const con = mongoose.connection
+
+// below used to open connection
+con.on('open', () => {
+    console.log('connected...')
+})
+
+// it is used to tell express framework that the 
+// i will be using json for express framework
+app.use(express.json())
+
+// its kind of variable for the given path/file
+const alienRouter = require('./routes/aliens')
+// for all the "/alients" request, sends the request to alienRouter 
+// variable which is defined above
+app.use('/aliens',alienRouter)
+
+app.listen(9000, () => {
+    console.log('Server started')
+})
